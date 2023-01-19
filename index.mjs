a6ab93ab852ae9f126e8c6e7c06fd3a78f7f329d
digiguru <digiguru@gmail.com>
2023-01-19 16:40:20 +0000

    Read params

--- index.mjs
+++ index.mjs
@@ -9,4 +9,15 @@ program
         console.log(chalk.red.bold('Welcome. Running version 0.1.0'))
     });
 
+program
+    .command('connect')
+    .description('Connects to a remote instace using a url, username & password')
+    .option('-s, --server <type>', 'the server to connect to')
+    .option('-u, --username <type>', 'your username - typically an email address')
+    .option('-p, --password <type>', 'you api token as setup in your jira porfolio on https://id.atlassian.com/manage-profile/security/api-tokens ', ',')
+    
+    .action(({server, username, password}) => {
+        console.log(chalk.red.bold(`Connect to ${server}. Using ${username} and ${password}`))
+    });
+
 program.parse()
