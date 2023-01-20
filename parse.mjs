ecabe4a15b549ba5dd8bdba2de2c040b675e641c
digiguru <digiguru@gmail.com>
2023-01-20 16:06:11 +0000

    Proper lookup class

--- parse.mjs
+++ parse.mjs
@@ -14,17 +14,23 @@ export function parseBlockers(data, key) {
     });
     return parseBlocker(targetIssue);
 }
-
+const lookup = {
+    key: {value:"key"},
+    additional: {value:"fields.customfield_10004", default: "unestimated"},
+    text: {value:"fields.summary"},
+    status: {value:"fields.status.name"},
+    grouping: {value:"fields.parent.fields.summary"}
+}
 export function parseBlocker(targetIssue) {
-    const key = resolvePath(targetIssue, "key");
-    const additional = resolvePath(targetIssue, "fields.customfield_10004") || "unestimated";
-    const text = resolvePath(targetIssue,"fields.summary") || key;
+    const key = resolvePath(targetIssue, lookup.key.value);
+    const additional = resolvePath(targetIssue, lookup.additional.value) || lookup.additional.default;
+    const text = resolvePath(targetIssue, lookup.text.value) || key;
     const keys =  {
         'blocks': [],
         'is blocked by': [],
         'key': key,
-        'grouping': resolvePath(targetIssue, "fields.parent.fields.summary"),
-        'status': resolvePath(targetIssue,"fields.status.name"),
+        'grouping': resolvePath(targetIssue, lookup.grouping.value),
+        'status': resolvePath(targetIssue, lookup.status.value),
         'title': text + (additional ? ` (${additional})` : ""),
     }
     if(targetIssue && targetIssue.fields) {
