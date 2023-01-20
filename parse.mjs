fc90a1670e8a29e8930d3fcaad15cebb134570b7
digiguru <digiguru@gmail.com>
2023-01-20 15:06:17 +0000

    Adds lookup capability to the object stuff

--- parse.mjs
+++ parse.mjs
@@ -1,12 +1,11 @@
-const keys = {
-    'blocks': [],
-    'is blocked by': [],
-    'key': `${targetIssue.key}`,
-    'grouping': `${targetIssue.fields?.parent?.fields?.summary}`,
-    //'epic': targetIssue.fields?.customfield_11100,
-    'status': `${targetIssue.fields?.status?.name}`,
-    'summary': `${targetIssue.fields?.summary || targetIssue.key} (${ targetIssue.fields?.customfield_10004 || "unestimated"})`
-};
+
+const resolvePath = function (object, path, defaultValue) {
+    return path
+        .split('.')
+        .reduce((o, p) => o ? o[p] : defaultValue, object)
+}
+
+
 
 export function parseBlockers(data, key) {
     const tickets = data.length ? data : data.issues;
@@ -17,7 +16,14 @@ export function parseBlockers(data, key) {
 }
 
 export function parseBlocker(targetIssue) {
-
+    const keys =  {
+        'blocks': [],
+        'is blocked by': [],
+        'key': resolvePath(targetIssue, "key"),
+        'grouping': resolvePath(targetIssue, "fields.parent.fields.summary"),
+        'status': `${targetIssue.fields?.status?.name}`,
+        'summary': `${targetIssue.fields?.summary || targetIssue.key} (${ targetIssue.fields?.customfield_10004 || "unestimated"})`
+    }
     if(targetIssue && targetIssue.fields) {
         if(targetIssue.fields.issuelinks) {
             targetIssue.fields.issuelinks.forEach( link => {
