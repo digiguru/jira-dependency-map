10f11980aec0594cc0aff50e4128a70c9db3146d
digiguru <digiguru@gmail.com>
2026-08-08 00:52:30 +0100

    Restore Jira CLI and remove unused callback index

--- jira.mjs
+++ jira.mjs
@@ -1,37 +1,47 @@
-import dotenv from 'dotenv';
-import { queryJira } from './queryJira.mjs';
-import { parseMultipleBlockers } from './parse.mjs';
-import { remapTickets } from './remapper.mjs';
 
-dotenv.config();
+import JiraApi from 'jira-client';
+import {remapTickets} from './remapper.mjs'
+import { parseMultipleBlockers } from "./parse.mjs";
+import {toDot} from './toDot.mjs';
 
-const {
-  JIRA_SERVER: server,
-  JIRA_USERNAME: username,
-  JIRA_PASSWORD: password,
-  JIRA_QUERY: query,
-  JIRA_NUMBER: number,
-} = process.env;
+export async function connect (args) {
+    const data = await queryJira(args);
+    const issues = data.issues;
+    console.log(`Query returns ${issues.length}`);
+}
+export async function raw (args) {
+  const data = await queryJira(args);
+  const issues = data.issues;
+  console.log(JSON.stringify(issues, null, 2));
+}
 
-const map = {
-  blockers: {
-    value: 'fields.customfield_10000',
-    default: [],
-  },
-  key: 'key',
-  summary: 'fields.summary',
-  status: 'fields.status.name',
-  type: 'fields.issuetype.name',
-};
+export async function remap (args) {
+  const tickets = await parseJira(args);
+  console.log(tickets);
+}
+export async function dot (args) {
+  const tickets = await parseJira(args);
+  const dot = toDot(tickets);
+  console.log(dot)
+}
+function connectJira({server, username, password}) {
+  return new JiraApi({
+    protocol: 'https',
+    host: server,
+    username: username,
+    password: password,
+    apiVersion: '3',
+    strictSSL: true
+  });
+}
 
-export async function getJiraDependencies({
-  server,
-  username,
-  password,
-  query,
-  number,
-  map,
-}) {
+async function queryJira({server, username, password, query, number}) {
+  const jira = connectJira({server, username, password, query});
+  const data = await jira.searchJira(query, {maxResults: number});
+  return data;
+}
+
+async function parseJira ({server, username, password, query, number, map}) {
   const data = await queryJira({server, username, password, query, number});
   let settings = {};
 
@@ -58,15 +68,4 @@ curl -D- \
    -X GET \
    -H "Content-Type: application/json" \
    https://your-domain.atlassian.net/rest/api/2/issue/createmeta
-*/
-
-if (process.argv[1] === new URL(import.meta.url).pathname) {
-  getJiraDependencies({
-    server,
-    username,
-    password,
-    query,
-    number,
-    map,
-  }).then(console.log);
-}
+   */
